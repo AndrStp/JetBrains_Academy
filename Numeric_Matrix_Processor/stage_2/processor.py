b23e7908dcbf2c058b7e61a334eba3a1ed173270
Andriy Stepanenko <47579173+AndrStp@users.noreply.github.com>
2021-09-18 21:05:32 +0300

    Add files via upload

---
+++ Numeric_Matrix_Processor/stage_2/processor.py
@@ -0,0 +1,65 @@
+from typing import List, Optional
+
+
+def main():
+    row, column = map(int, input().split())
+    m1 = make_matrix(row)
+    const = int(input())
+    m_mult = multiply_matrix(m1, const)
+    display_matrix(m_mult)
+    # row, column = map(int, input().split())
+    # m2 = make_matrix(row)
+    # m_sum = add_matrices(m1, m2)
+    # if m_sum:
+    #     display_matrix(m_sum)
+    # else:
+    #     print('ERROR')
+
+
+def make_matrix(row: int) -> List[list]:
+    """Return the matrix with given n rows"""
+    return [list(map(int, input().split())) for _ in range(row)]
+
+
+def add_matrices(matrix_1: List[list], matrix_2: List[list]) -> Optional[List[list]]:
+    """Return the matrix by adding two matrices"""
+    if not check_matrices(matrix_1, matrix_2):
+        return None
+
+    matrix = []
+    for row_1, row_2 in zip(matrix_1, matrix_2):
+        matrix_row = []
+        for cell_1, cell_2 in zip(row_1, row_2):
+            matrix_row.append(cell_1 + cell_2)
+        matrix.append(matrix_row)
+    return matrix
+
+
+def multiply_matrix(matrix: List[list], constant: int) -> List[list]:
+    """Return the result of multiplying a matrix by a constant"""
+    new_matrix = []
+    for row in matrix:
+        row_ = [cell * constant for cell in row]
+        new_matrix.append(row_)
+    return new_matrix
+
+
+def check_matrices(matrix_1: List[list], matrix_2: List[list]) -> bool:
+    """Returns whether the given matrices are of equal size"""
+    matrix_1_row_num, matrix_1_col_num = len(matrix_1), len(matrix_1[0])
+    matrix_2_row_num, matrix_2_col_num = len(matrix_2), len(matrix_2[0])
+    if (matrix_1_row_num, matrix_1_col_num) != (matrix_2_row_num, matrix_2_col_num):
+        return False
+    return True
+
+
+def display_matrix(matrix: List[list]) -> None:
+    """Displays the matrix"""
+    for row in matrix:
+        for cell in row:
+            print(cell, end=' ')
+        print()
+
+
+if __name__ == '__main__':
+    main()
